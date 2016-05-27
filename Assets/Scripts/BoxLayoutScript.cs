64f06e8cae02d3dd0eb82dc17b7533dc3d75670f
Joachim Fritscher <joachim.fritscher@student.uni-tuebingen.de>
2016-05-27 13:10:46 +0200

    help box resizing

---
+++ Assets/Scripts/BoxLayoutScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxLayoutScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("HelpBackground"))
+        {
+            Image background = gameObject.GetComponent<Image>();
+            Text text = gameObject.GetComponentInChildren<Text>();
+            Debug.Log(text.rectTransform.rect.height);
+            background.rectTransform.sizeDelta = new Vector2(text.rectTransform.rect.width, text.rectTransform.rect.height);
+            Debug.Log(background.rectTransform.rect.height);
+        }
+	}
+}
