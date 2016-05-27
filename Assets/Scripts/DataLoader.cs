05472beb46783bb897b1e35a647b8e3f61c86308
Joachim Fritscher <joachim.fritscher@student.uni-tuebingen.de>
2016-05-27 10:39:02 +0200

    commit with registration scene

---
+++ Assets/Scripts/DataLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataLoader : MonoBehaviour {
+	// Use this for initialization
+    void Start()
+    {
+        string username = "";
+        //yield return GetPassword(username);
+        
+    }
+
+    public IEnumerator GetPassword(string username)
+    {
+        Debug.Log("load Data");
+        WWW itemsData = new WWW("http://localhost/unity_games/users_data.php?field=" + username);
+
+        yield return itemsData;
+
+        if (itemsData.text != "")
+        {
+            string[] data = itemsData.text.Split('|');
+            print(data[1]);
+        } else
+        {
+            print("Name Can be Used");
+        }
+        
+        
+        Debug.Log(itemsData.text);
+    }
+}
---
+++ Assets/Scripts/RegisterScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine.UI;
+
+public class RegisterScript : MonoBehaviour {
+    private bool usernameValid;
+    
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnRegisterButtonClick()
+    {
+        string username = GameObject.Find("UsernameFieldText").GetComponent<Text>().text;
+        string password = GameObject.Find("PasswordFieldText").GetComponent<Text>().text;
+        string sex = ExtractGenderFromToggle(
+            GameObject.Find("MaleToggle").GetComponent<Toggle>() as Toggle,
+            GameObject.Find("FemaleToggle").GetComponent<Toggle>() as Toggle);
+        string age = GameObject.Find("AgeFieldText").GetComponent<Text>().text;
+        string school = GameObject.Find("SchoolFieldText").GetComponent<Text>().text;
+        string state = GameObject.Find("StateFieldText").GetComponent<Text>().text;
+        string mothertongue = GameObject.Find("MothertongueFieldText").GetComponent<Text>().text;
+    }
+
+    private static string ExtractGenderFromToggle(Toggle maleToggle, Toggle femaleToggle)
+    {
+        if (maleToggle.isOn && !femaleToggle.isOn)
+        {
+            return "M";
+        }
+        else if (!maleToggle.isOn && femaleToggle.isOn)
+        {
+            return "W";
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    public static string GetSHA256(string text)
+    {
+        byte[] byteRepresentation = Encoding.UTF8.GetBytes(text);
+
+        SHA256Managed hashstring = new SHA256Managed();
+        byte[] hash = hashstring.ComputeHash(byteRepresentation);
+        string hashString = string.Empty;
+        foreach (byte x in hash)
+        {
+            hashString += string.Format("{0:x2}", x);
+        }
+        return hashString;
+    }
+}
