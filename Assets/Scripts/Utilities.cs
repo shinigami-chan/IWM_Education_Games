e661246d38531c8d92a1253bb45698ec2cab9369
shinigami-chan <s.hornuff@web.de>
2016-06-24 15:21:07 +0200

    Utilities script and Unity Guide

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class Hashing {
+
+    public static string GetSHA256(string text)
+    {
+        byte[] byteRepresentation = Encoding.UTF8.GetBytes(text);
+
+        SHA256Managed hashstring = new SHA256Managed();
+        byte[] hash = hashstring.ComputeHash(byteRepresentation);
+        string hashString = string.Empty;
+        foreach (byte x in hash)
+        {
+            hashString += string.Format("{0:x2}", x);
+        }
+        return hashString;
+    }
+}
