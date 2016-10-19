098c4cae391c9a899d5e4a341550f297ff0bcca5
shinigami-chan <s.hornuff@web.de>
2016-10-19 12:18:56 +0200

    Floating change and change in projecct settings (default fullscreen)

--- Assets/Scripts/BalloonScripts/Floating.cs
+++ Assets/Scripts/BalloonScripts/Floating.cs
@@ -2,15 +2,27 @@
 using System.Collections;
 using Random = System.Random;
 using System;
+using UnityEngine.SceneManagement;
 
 public class Floating : MonoBehaviour {
 
     private float step;
     private float offsetY; //How far to offset the object upwards
     private float offsetX;
-         
+
+    private float amplitude;
+
         void Start()
     {
+        if(SceneManager.GetActiveScene().name != "Balloon_Game")
+        {
+            amplitude = 10 * GameObject.FindObjectOfType<Canvas>().GetComponent<Canvas>().transform.localScale.y;
+        }
+        else
+        {
+            amplitude = GameObject.FindObjectOfType<Canvas>().GetComponent<Canvas>().transform.localScale.y / 100;
+        }
+
         Random rnd = new Random(Guid.NewGuid().GetHashCode());
         //Create an offset based on our height
         offsetY = transform.position.y + transform.localScale.y;
@@ -32,7 +44,7 @@ public class Floating : MonoBehaviour {
 
         //Float up and down along the y axis, 
         Vector3 point = transform.position;
-        point.y = Mathf.Sin(step) * 0.5f + offsetY;
+        point.y = offsetY + Mathf.Sin(step) * amplitude; //0.5f / Screen.height*1000;
         transform.position = point;
     }
 }
